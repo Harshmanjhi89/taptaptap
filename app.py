119e6b5b0ea96b3c1bb300b0e777f180051d62e9
Harshmanjhi89 <harshmanjhi180+2@gmail.com>
2024-10-30 08:54:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# app.py
+
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='play')  # Set the templates folder to 'play'
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # It will look for 'play/index.html'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
