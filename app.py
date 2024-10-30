720c349fa1582f344aac5bf58ddb5e9ab4371d7b
Harshmanjhi89 <harshmanjhi180+2@gmail.com>
2024-10-30 11:21:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Route for the main HTML page
 @app.route('/')
 def home():
-    return send_file('index.html')
+    return render_template('index.html')
+
+# Route for the first play HTML page
+@app.route('/play')
+def play1():
+    return render_template('play/index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
