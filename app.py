4cdfc20d1ec1025da8fe211718f568b08f8a3a85
Harshmanjhi89 <harshmanjhi180+2@gmail.com>
2024-10-30 11:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, send_from_directory, send_file
+from flask import Flask, send_from_directory, redirect
+import os
 
 app = Flask(__name__)
 
 # Route for the main HTML page
 @app.route('/')
 def index():
-    return send_file('index.html')
+    return redirect('/play')  # Redirect to the game page
 
 @app.route('/play')  # Make sure this starts with a slash
 def play():
     return send_from_directory('play', 'index.html')
 
-
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-   
